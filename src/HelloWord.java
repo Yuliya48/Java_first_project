48a78a53d84c61d7842f93520e8b907d149b80e6
Yuliya48 <kolyabina8@gmail.com>
2022-12-09 08:58:04 -0500

    add new lines

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,5 +2,11 @@ public class HelloWord {
 
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        System.out.println("Hello world 1!!!");
+        System.out.println("Hello world 2!!!");
+        System.out.println("Hello world 3!!!");
+
+
     }
+
 }
